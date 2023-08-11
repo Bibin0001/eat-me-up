483b4767de08fc8908180bcdd7d29141fa37c4ec
b1bin <bobo692233@gmail.com>
2023-08-11 22:03:10 +0300

    Fixed a bug

--- eat_me_next_tri/settings.py
+++ eat_me_next_tri/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
 
-ALLOWED_HOSTS = ['172.18.0.4', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 # if ALLOWED_HOSTS_ENV:
 #     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
@@ -97,7 +97,7 @@ DATABASES = {
         "NAME": "eat_me_db",
         "USER": "bobo",
         "PASSWORD": "bobo",
-        "HOST": "postgres",
+        "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
